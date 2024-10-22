ddb6b027f7618f0c2dd0d7b802d8d9184899ac36
aghabidareh <aghabidareh@gmail.com>
2024-10-22 07:11:19 -0700

    speed match between python and numpy

---
+++ ListVsNumpy.py
@@ -0,0 +1,23 @@
+import numpy as np
+from time import time
+
+def pythonVersion():
+    t1 = time()
+    x = range(100_000_000)
+    y = range(100_000_000)
+    z = []
+    for i in range(100_000_000):
+        z.append(x[i] + y[i])
+    t2 = time()
+    return t2 - t1
+
+def numpyVersion():
+    t1 = time()
+    x = np.arange(100_000_000)
+    y = np.arange(100_000_000)
+    z = x + y
+    t2 = time()
+    return t2 - t1
+
+print(f'time for python : {pythonVersion()}')
+print(f'time for numpy : {numpyVersion()}')
