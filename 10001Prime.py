adfea76dc3d54f21995b87406aec5e573daa0c10
aghabidareh <aghabidareh@gmail.com>
2024-10-27 17:36:35 +0330

    10001 prime number

---
+++ 10001Prime.py
@@ -0,0 +1,20 @@
+import math
+
+def isPrime(n):
+    for i in range(2 , int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+primes = []
+counter = 0
+i = 2
+
+while True:
+    if isPrime(i):
+        counter += 1
+        primes.append(i)
+    i += 1
+    if counter >= 10_001:
+        print(primes[-1])
+        break
