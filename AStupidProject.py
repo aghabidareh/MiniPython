16583a141ee5b1c950d3a4a1bf914a7df9f42427
aghabidareh <aghabidareh@gmail.com>
2024-11-02 18:18:57 +0330

    stupidy

---
+++ AStupidProject.py
@@ -0,0 +1,7 @@
+string = input('enter a string: ')
+lists = ['a' , 'g' , 'h' , 'a' , 'b' , 'i' , 'd' , 'a' , 'r' , 'e' , 'h']
+
+if string in lists:
+    print(string + ' ' + ''.join(lists))
+else:
+    print(''.join(lists))
