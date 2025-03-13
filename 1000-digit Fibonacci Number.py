d0ca04a40571887fe5995a3259745de25f76ea25
aghabidareh <aghabidareh@gmail.com>
2025-03-13 07:16:29 -0700

    1000-digit Fibonacci Number

---
+++ 1000-digit Fibonacci Number.py
@@ -0,0 +1,13 @@
+def find_first_fibonacci_with_1000_digits():
+    a, b = 1, 1
+    index = 2
+
+    while True:
+        a, b = b, a + b
+        index += 1
+
+        if len(str(b)) >= 1000:
+            return index
+
+result = find_first_fibonacci_with_1000_digits()
+print(result)
