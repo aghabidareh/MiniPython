0aa46204800f249bee2dc3f184822b3b2ac33c8a
aghabidareh <aghabidareh@gmail.com>
2025-03-13 03:58:33 -0700

    cleaning

--- LoginForm.py
+++ LoginForm.py
@@ -1,37 +1,38 @@
 import tkinter as tk
 from tkinter import messagebox
 
+
 def login():
     username = entry1.get()
     password = entry2.get()
-    
-    if (username == "" and password==""):
-        messagebox.showinfo("" , "Blank Not Allowed")
-    
-    elif (username=="Aghabidareh" and password=="aghabidareh"):
-        messagebox.showinfo("" , "You Are LogedIn Now")
-        
+
+    if (username == "" and password == ""):
+        messagebox.showinfo("", "Blank Not Allowed")
+
+    elif (username == "Aghabidareh" and password == "aghabidareh"):
+        messagebox.showinfo("", "You Are LogedIn Now")
+
     else:
-        messagebox.showinfo("" , "Incorrect Username or Password")
+        messagebox.showinfo("", "Incorrect Username or Password")
+
 
 root = tk.Tk()
 root.title('Aghabidareh \t | \t Login')
 root.geometry('300x200')
-root.resizable(False , False)
+root.resizable(False, False)
 
 global entry1
 global entry2
 
-tk.Label(root, text="Usesrname").place(x=20,y=20)
-tk.Label(root, text="Password").place(x=20,y=70)
+tk.Label(root, text="Usesrname").place(x=20, y=20)
+tk.Label(root, text="Password").place(x=20, y=70)
 
+entry1 = tk.Entry(root, bd=5)
+entry1.place(x=140, y=20)
 
-entry1 = tk.Entry(root , bd=5)
-entry1.place(x=140,y=20)
+entry2 = tk.Entry(root, bd=5)
+entry2.place(x=140, y=70)
 
-entry2 = tk.Entry(root , bd=5)
-entry2.place(x=140,y=70)
-
-tk.Button(root, text="Submit" , command=login , height=3 , width=13 , bd=6).place(x=100,y=120)
+tk.Button(root, text="Submit", command=login, height=3, width=13, bd=6).place(x=100, y=120)
 
 root.mainloop()
