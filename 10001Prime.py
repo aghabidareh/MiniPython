be4fb2badc9a695dba624b5132cab8bc79915f06
aghabidareh <aghabidareh@gmail.com>
2025-03-13 03:57:04 -0700

    cleaning

--- 10001Prime.py
+++ 10001Prime.py
@@ -1,10 +1,12 @@
 import math
 
+
 def isPrime(n):
-    for i in range(2 , int(math.sqrt(n)) + 1):
+    for i in range(2, int(math.sqrt(n)) + 1):
         if n % i == 0:
             return False
     return True
+
 
 primes = []
 counter = 0
