969a6f88f8525f2fefdce4f8efaae7ee186afb73
aghabidareh <aghabidareh@gmail.com>
2025-03-13 07:11:55 -0700

    Lexicographic Permutations

---
+++ Lexicographic Permutations.py
@@ -0,0 +1,17 @@
+import math
+
+def find_lexicographic_permutation(digits, target):
+    permutation = []
+    target -= 1
+    while digits:
+        n = len(digits)
+        factorial = math.factorial(n - 1)
+        index = target // factorial
+        permutation.append(digits.pop(index))
+        target %= factorial
+    return ''.join(permutation)
+
+digits = [str(i) for i in range(10)]
+
+result = find_lexicographic_permutation(digits, 1000000)
+print(result)
