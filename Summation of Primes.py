2d95296fa11344e85223407030f9300a1729e49e
aghabidareh <aghabidareh@gmail.com>
2025-03-13 04:16:58 -0700

    cleaning

--- Summation of Primes.py
+++ Summation of Primes.py
@@ -2,12 +2,13 @@ def sieve_of_eratosthenes(limit):
     is_prime = [True] * (limit + 1)
     is_prime[0] = is_prime[1] = False
 
-    for p in range(2 , int(limit ** 0.5) + 1):
+    for p in range(2, int(limit ** 0.5) + 1):
         if is_prime[p]:
             for multiple in range(p * p, limit + 1, p):
                 is_prime[multiple] = False
 
     return [p for p, prime in enumerate(is_prime) if prime]
+
 
 limit = 2000000
 
