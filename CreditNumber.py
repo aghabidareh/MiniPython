13e1f596d5915a0758f7eafeeab6ddafca318102
aghabidareh <aghabidareh@gmail.com>
2025-03-13 03:57:48 -0700

    cleaning

--- CreditNumber.py
+++ CreditNumber.py
@@ -1,9 +1,9 @@
 creditNumber = input("Enter your credit number: ")
 
-if(len(creditNumber) < 16):
+if (len(creditNumber) < 16):
     print("Your credit number is invalid.")
     exit()
 
-creditNumber = creditNumber.replace(creditNumber[:-4] , len(creditNumber[:-4]) * '*')
+creditNumber = creditNumber.replace(creditNumber[:-4], len(creditNumber[:-4]) * '*')
 
 print('your credit number is', creditNumber)
