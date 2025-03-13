1541b25e8ecedd471c3b68c5a1d01c04966441e2
aghabidareh <aghabidareh@gmail.com>
2025-03-13 07:09:43 -0700

    Non-Abundant Sums

--- Non-Abundant Sums.py
+++ Non-Abundant Sums.py
@@ -12,12 +12,14 @@ def sum_of_proper_divisors(n):
         i += 1
     return total
 
+
 def find_abundant_numbers(limit):
     abundant_numbers = []
     for n in range(1, limit + 1):
         if sum_of_proper_divisors(n) > n:
             abundant_numbers.append(n)
     return abundant_numbers
+
 
 def generate_abundant_sums(abundant_numbers, limit):
     abundant_sums = set()
@@ -29,6 +31,7 @@ def generate_abundant_sums(abundant_numbers, limit):
             abundant_sums.add(sum_abundant)
     return abundant_sums
 
+
 def sum_of_non_abundant_sums(limit):
     abundant_numbers = find_abundant_numbers(limit)
     abundant_sums = generate_abundant_sums(abundant_numbers, limit)
@@ -37,6 +40,7 @@ def sum_of_non_abundant_sums(limit):
         if n not in abundant_sums:
             total += n
     return total
+
 
 limit = 28123
 
