70ebd332c370f4a60f69c43fd176192dd028b99b
aghabidareh <aghabidareh@gmail.com>
2025-03-13 07:07:50 -0700

    Names Scores

--- Names Scores.py
+++ Names Scores.py
@@ -2,6 +2,7 @@
 def alphabetical_value(name):
     return sum(ord(char) - ord('A') + 1 for char in name)
 
+
 with open('names.txt', 'r') as file:
     names = file.read().replace('"', '').split(',')
 
