24793cd883fda61704adc92e6d8876984b4cce11
aghabidareh <aghabidareh@gmail.com>
2025-03-15 02:49:00 -0700

    Pentagon Numbers

--- Pentagon Numbers.py
+++ Pentagon Numbers.py
@@ -2,6 +2,7 @@ def is_pentagonal(x):
     n = (1 + (1 + 24 * x) ** 0.5) / 6
     return n == int(n)
 
+
 def generate_pentagonal_numbers(limit):
     pentagonal_numbers = []
     n = 1
@@ -12,6 +13,7 @@ def generate_pentagonal_numbers(limit):
         pentagonal_numbers.append(p_n)
         n += 1
     return pentagonal_numbers
+
 
 def find_minimal_difference():
     pentagonal_numbers = []
@@ -25,6 +27,7 @@ def find_minimal_difference():
             if is_pentagonal(sum_p) and is_pentagonal(diff_p):
                 return diff_p
         n += 1
+
 
 result = find_minimal_difference()
 
