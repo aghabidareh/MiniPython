19992761094b8174fb68ea02cf245b26990e09ca
aghabidareh <aghabidareh@gmail.com>
2025-03-14 23:21:22 -0700

    Integer Right Triangles

--- Integer Right Triangles.py
+++ Integer Right Triangles.py
@@ -1,10 +1,12 @@
 import math
 from collections import defaultdict
 
+
 def gcd(a, b):
     while b:
         a, b = b, a % b
     return a
+
 
 def find_max_solutions_perimeter(limit):
     perimeter_counts = defaultdict(int)
@@ -12,9 +14,9 @@ def find_max_solutions_perimeter(limit):
     for m in range(2, int(math.sqrt(limit)) + 1):
         for n in range(1, m):
             if (m + n) % 2 == 1 and gcd(m, n) == 1:
-                a = m**2 - n**2
+                a = m ** 2 - n ** 2
                 b = 2 * m * n
-                c = m**2 + n**2
+                c = m ** 2 + n ** 2
                 p = a + b + c
 
                 k = 1
@@ -24,6 +26,7 @@ def find_max_solutions_perimeter(limit):
 
     max_p = max(perimeter_counts, key=lambda x: perimeter_counts[x])
     return max_p
+
 
 limit = 1000
 result = find_max_solutions_perimeter(limit)
