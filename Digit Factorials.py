4ca50c226bc3740eb1978effa4212770168acc1a
aghabidareh <aghabidareh@gmail.com>
2025-03-14 23:13:55 -0700

    Digit Factorials

---
+++ Digit Factorials.py
@@ -0,0 +1,23 @@
+import math
+
+def sum_of_factorial_of_digits(n):
+    return sum(math.factorial(int(digit)) for digit in str(n))
+
+def find_factorions():
+    factorions = []
+
+    factorial = [math.factorial(i) for i in range(10)]
+
+    upper_limit = 7 * factorial[9]
+
+    for num in range(10, upper_limit + 1):
+        if num == sum_of_factorial_of_digits(num):
+            factorions.append(num)
+
+    return factorions
+
+factorions = find_factorions()
+
+result = sum(factorions)
+
+print(f"The sum of all numbers equal to the sum of the factorial of their digits is: {result}")
