d0b7020262d2c1e5f0500462bdcb6f8d5a8099bb
aghabidareh <aghabidareh@gmail.com>
2025-03-15 02:47:41 -0700

    Sub-string Divisibility

--- Sub-string Divisibility.py
+++ Sub-string Divisibility.py
@@ -1,17 +1,20 @@
 import itertools
 
+
 def is_pandigital(number):
     digits = set(str(number))
     return len(digits) == 10 and len(str(number)) == 10
+
 
 def satisfies_conditions(number):
     s = str(number)
     divisors = [2, 3, 5, 7, 11, 13, 17]
     for i in range(1, 8):
-        substring = int(s[i:i+3])
-        if substring % divisors[i-1] != 0:
+        substring = int(s[i:i + 3])
+        if substring % divisors[i - 1] != 0:
             return False
     return True
+
 
 def find_pandigital_numbers():
     digits = "0123456789"
@@ -19,6 +22,7 @@ def find_pandigital_numbers():
 
     valid_numbers = [int(num) for num in pandigital_numbers if satisfies_conditions(num)]
     return valid_numbers
+
 
 valid_numbers = find_pandigital_numbers()
 
