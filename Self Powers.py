2537e8aaabaed5d44ee14a7f9f5b14b801c920f0
aghabidareh <aghabidareh@gmail.com>
2025-03-15 02:55:50 -0700

    Self Powers

--- Self Powers.py
+++ Self Powers.py
@@ -1,9 +1,10 @@
 def last_ten_digits():
-    mod = 10**10
+    mod = 10 ** 10
     total = 0
     for n in range(1, 1001):
         total = (total + pow(n, n, mod)) % mod
     return total
+
 
 result = last_ten_digits()
 
