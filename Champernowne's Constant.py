08f0fbe766980592193853fba4d173b318945638
aghabidareh <aghabidareh@gmail.com>
2025-03-14 23:22:45 -0700

    Champernowne's Constant

--- Champernowne's Constant.py
+++ Champernowne's Constant.py
@@ -6,17 +6,20 @@ def construct_fraction(limit):
         num += 1
     return fraction
 
+
 def get_digits(fraction, positions):
     digits = []
     for pos in positions:
         digits.append(int(fraction[pos - 1]))
     return digits
 
+
 def calculate_product(digits):
     product = 1
     for digit in digits:
         product *= digit
     return product
+
 
 positions = [1, 10, 100, 1000, 10000, 100000, 1000000]
 
