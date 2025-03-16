7a9f7b3586b65bf799c61bbd72d53b120804e025
aghabidareh <aghabidareh@gmail.com>
2025-03-16 00:01:37 -0700

    Poker Hands

--- Poker Hands.py
+++ Poker Hands.py
@@ -1,6 +1,5 @@
 import requests
 
-# Define card values and ranks
 card_values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'T': 10, 'J': 11, 'Q': 12, 'K': 13,
                'A': 14}
 ranks = ['High Card', 'One Pair', 'Two Pairs', 'Three of a Kind', 'Straight', 'Flush', 'Full House', 'Four of a Kind',
