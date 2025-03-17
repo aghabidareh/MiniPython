7914f4eb64d913ff5769ec8882dcf0ef2f599866
aghabidareh <aghabidareh@gmail.com>
2025-03-17 04:51:12 -0700

    Powerful Digit Counts

---
+++ Powerful Digit Counts.py
@@ -0,0 +1,19 @@
+import math
+
+def count_n_digit_nth_powers():
+    count = 0
+    n = 1
+    while True:
+        lower_bound = 10 ** ((n - 1) / n)
+        if lower_bound > 9:
+            break
+        a_min = math.ceil(lower_bound)
+        a_max = 9
+        count += (a_max - a_min + 1)
+        n += 1
+    return count
+
+# Count the number of n-digit positive integers which are also nth powers
+result = count_n_digit_nth_powers()
+
+print(f"The number of n-digit positive integers which are also nth powers is: {result}")
