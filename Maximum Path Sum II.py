a03e6bc8325b4e5fc624117514da245f426f1911
aghabidareh <aghabidareh@gmail.com>
2025-03-17 22:51:53 -0700

    Maximum Path Sum II

---
+++ Maximum Path Sum II.py
@@ -0,0 +1,20 @@
+def read_triangle(file_path):
+    with open(file_path, 'r') as file:
+        triangle = [[int(num) for num in line.split()] for line in file]
+    return triangle
+
+def max_total(triangle):
+    # Start from the second last row and move upwards
+    for i in range(len(triangle) - 2, -1, -1):
+        for j in range(len(triangle[i])):
+            triangle[i][j] += max(triangle[i + 1][j], triangle[i + 1][j + 1])
+    return triangle[0][0]
+
+# Read the triangle from the file
+file_path = '0067_triangle.txt'
+triangle = read_triangle(file_path)
+
+# Find the maximum total from top to bottom
+result = max_total(triangle)
+
+print(f"The maximum total from top to bottom is: {result}")
