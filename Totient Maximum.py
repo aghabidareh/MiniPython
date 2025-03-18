c82a027bf6d68e1fc48acb36d33a8ead05675009
aghabidareh <aghabidareh@gmail.com>
2025-03-18 09:15:37 -0700

    Totient Maximum

---
+++ Totient Maximum.py
@@ -0,0 +1,16 @@
+import sympy
+
+def find_optimal_n(limit):
+    primes = list(sympy.primerange(1, limit))
+    n = 1
+    for p in primes:
+        if n * p > limit:
+            break
+        n *= p
+    return n
+
+# Find the value of n <= 1,000,000 for which n / phi(n) is maximized
+limit = 1000000
+result = find_optimal_n(limit)
+
+print(f"The value of n <= 1,000,000 for which n / phi(n) is maximized is: {result}")
