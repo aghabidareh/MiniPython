2f76e48b288fc53ba9222c7c8f95eb7e8f72a0ad
aghabidareh <aghabidareh@gmail.com>
2025-03-17 22:52:05 -0700

    Maximum Path Sum II

--- Maximum Path Sum II.py
+++ Maximum Path Sum II.py
@@ -4,7 +4,6 @@ def read_triangle(file_path):
     return triangle
 
 def max_total(triangle):
-    # Start from the second last row and move upwards
     for i in range(len(triangle) - 2, -1, -1):
         for j in range(len(triangle[i])):
             triangle[i][j] += max(triangle[i + 1][j], triangle[i + 1][j + 1])
