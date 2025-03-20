aabdbf14cab3be46809b113bf05dec581c26f20a
aghabidareh <aghabidareh@gmail.com>
2025-03-20 10:31:51 -0700

    Coin Partitions

--- Coin Partitions.py
+++ Coin Partitions.py
@@ -1,6 +1,6 @@
 def find_least_n_divisible_by_one_million():
     target = 1000000
-    p = [0] * (10**6)  # Adjust the size if necessary
+    p = [0] * (10**6)
     p[0] = 1
     n = 1
     while True:
@@ -11,7 +11,6 @@ def find_least_n_divisible_by_one_million():
             sign = (-1) ** (i - 1)
             p_n += sign * p[n - pentagonal]
             i += 1
-            # Generate the next pentagonal number
             k = (i // 2) + 1 if i % 2 == 0 else -(i // 2) - 1
             pentagonal = k * (3 * k - 1) // 2
         p[n] = p_n % target
