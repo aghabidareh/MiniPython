0810cb8e170a6a38c6d207b35eedf6d6e01ff983
aghabidareh <aghabidareh@gmail.com>
2025-03-21 07:48:12 -0700

    Path Sum: Two Ways

--- Path Sum-Two Ways.py
+++ Path Sum-Two Ways.py
@@ -15,15 +15,12 @@ def minimal_path_sum(matrix):
     dp = [[0 for _ in range(cols)] for _ in range(rows)]
     dp[0][0] = matrix[0][0]
 
-    # Fill the first row
     for j in range(1, cols):
         dp[0][j] = dp[0][j - 1] + matrix[0][j]
 
-    # Fill the first column
     for i in range(1, rows):
         dp[i][0] = dp[i - 1][0] + matrix[i][0]
 
-    # Fill the rest of the table
     for i in range(1, rows):
         for j in range(1, cols):
             dp[i][j] = matrix[i][j] + min(dp[i - 1][j], dp[i][j - 1])
