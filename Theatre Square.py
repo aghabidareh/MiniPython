82a7d700ba05ae6ca6771a32a8ab1ae83f238106
aghabidareh <aghabidareh@gmail.com>
2025-03-21 07:43:19 -0700

    Theatre Square

--- Theatre Square.py
+++ Theatre Square.py
@@ -1,9 +1,7 @@
 import math
 
-# Read input values
 n, m, a = map(int, input().split())
 
-# Calculate the number of flagstones needed
 flagstones_n = math.ceil(n / a)
 flagstones_m = math.ceil(m / a)
 
