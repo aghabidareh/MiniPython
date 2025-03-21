b97fbb1bc2bddea2147b711d525ff0075385fb84
aghabidareh <aghabidareh@gmail.com>
2025-03-21 07:58:37 -0700

    Counting Rectangles

--- Counting Rectangles.py
+++ Counting Rectangles.py
@@ -8,7 +8,6 @@ best_m, best_n = 0, 0
 for m in range(1, 100):
     rect_m = (m * (m + 1)) // 2  # Sum of first m natural numbers
 
-    # Solve for n where (n * (n + 1)) / 2 is approximately target / rect_m
     n_estimate = int(((8 * target / rect_m + 1) ** 0.5 - 1) / 2)
 
     for n in [n_estimate, n_estimate + 1]:
