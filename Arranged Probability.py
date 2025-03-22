778e80217b0a8057b0340a8396a426253c4b19d7
aghabidareh <aghabidareh@gmail.com>
2025-03-22 05:18:17 -0700

    Arranged Probability

--- Arranged Probability.py
+++ Arranged Probability.py
@@ -6,9 +6,6 @@ def find_minimal_blue_discs():
     b = 15
     n = 21
 
-    # Recurrence relations for Pell's equation
-    # b_{k+1} = 3*b_k + 2*n_k - 2
-    # n_{k+1} = 4*b_k + 3*n_k - 3
 
     while n <= 10 ** 12:
         b_new = 3 * b + 2 * n - 2
@@ -18,7 +15,6 @@ def find_minimal_blue_discs():
     return b
 
 
-# Find the number of blue discs in the first arrangement with over 10^12 discs
 result = find_minimal_blue_discs()
 
 print(f"The number of blue discs is: {result}")
