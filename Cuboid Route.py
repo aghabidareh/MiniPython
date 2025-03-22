862137e69026f284e27242464c27197e990d1f3e
aghabidareh <aghabidareh@gmail.com>
2025-03-22 05:17:27 -0700

    Cuboid Route

--- Cuboid Route.py
+++ Cuboid Route.py
@@ -1,17 +1,18 @@
 import math
 
+
 def count_integer_paths(M):
     count = 0
     for a in range(1, M + 1):
         for b in range(a, M + 1):
-            min_k = math.ceil(math.sqrt(a**2 + (b + a)**2))
-            max_k = math.floor(math.sqrt(a**2 + (b + M)**2))
+            min_k = math.ceil(math.sqrt(a ** 2 + (b + a) ** 2))
+            max_k = math.floor(math.sqrt(a ** 2 + (b + M) ** 2))
             for k in range(min_k, max_k + 1):
-                c_squared = k**2 - a**2
-                if c_squared < (b)**2:
+                c_squared = k ** 2 - a ** 2
+                if c_squared < (b) ** 2:
                     continue
                 c_plus_b = math.isqrt(c_squared)
-                if c_plus_b**2 != c_squared:
+                if c_plus_b ** 2 != c_squared:
                     continue
                 c = c_plus_b - b
                 if c < b:
@@ -21,6 +22,7 @@ def count_integer_paths(M):
                 count += 1
     return count
 
+
 def find_min_M(target):
     M = 1
     while True:
@@ -28,6 +30,7 @@ def find_min_M(target):
         if count > target:
             return M
         M += 1
+
 
 target = 1000000
 result = find_min_M(target)
