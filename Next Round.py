9f84409eeb23d80a2e56bc7f1dd49170b95dc110
aghabidareh <aghabidareh@gmail.com>
2025-03-23 01:39:26 -0700

    Next Round

---
+++ Next Round.py
@@ -0,0 +1,6 @@
+n , k = map(int, input().split())
+scores = list(map(int, input().split()))
+
+threshold = scores[k-1]
+advancing = sum(1 for score in scores if score >= threshold and score > 0)
+print(advancing)
