746745542ccff2e6b976bf9a68f8105c38f1d336
aghabidareh <aghabidareh@gmail.com>
2025-04-09 20:02:03 +0330

    Stones on the Table

--- Stones on the Table.py
+++ Stones on the Table.py
@@ -1,8 +1,8 @@
-num_char , characters = int(input()), input()
-counter= 0
+num_char, characters = int(input()), input()
+counter = 0
 
-for i in range(num_char-1):
-    if characters[i] == characters[i+1]:
+for i in range(num_char - 1):
+    if characters[i] == characters[i + 1]:
         counter += 1
 
 print(counter)
